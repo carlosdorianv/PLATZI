e9854a7038521bf105ef4fbecd0e21b564f2e3fe
Carlos Vargas <carlosdorianv@gmail.com>
2022-07-27 11:51:06 -0500

    Utilizar archivos en python e incio de re final

---
+++ Cursos/CURSO DE PYTHON INTERMEDIO/curso-python/archivos.py
@@ -0,0 +1,29 @@
+import numbers
+
+
+def read():
+    numbers = []
+    with open("./archivos/numbres.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+def write():
+    names = ["Facundo", "Miguel", "Pepe", "Christian","Rocío"]
+    with open("./archivos/names.txt", "a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+
+
+
+def run():
+    write()
+
+
+
+
+
+if __name__ == "__main__":
+    run()
